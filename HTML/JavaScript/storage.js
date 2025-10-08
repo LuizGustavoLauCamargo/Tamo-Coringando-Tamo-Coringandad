79e78fa2e5646f67b2713775760b55690db1cc0b
Leolfc <leonardofeliciano81@gmail.com>
2025-10-08 14:22:52 -0300

    conectando mongodb

--- HTML/JavaScript/storage.js
+++
@@ -1,8 +0,0 @@
-export function salvarDados(chave, dados) {
-    localStorage.setItem(chave, JSON.stringify(dados));
-}
-
-export function carregarDados(chave) {
-    const dadosSalvos = localStorage.getItem(chave);
-    return dadosSalvos ? JSON.parse(dadosSalvos) : null;
-}
